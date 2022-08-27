a876163ed21a3bede788e6cf009676446c06a53f
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-27 19:32:10 +0600

    small modify

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
 var carObject = {
-    vehicle: "Car",
-    imageUrl: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    farePerKilo: 3,
-    capacity: 4,
-    description: "You Have More Flexibility And Freedom. When you travel by car, you are on your schedule and have complete control over your trip.",
-};
-var bikeObject = {
-    vehicle: "Bike",
-    imageUrl: "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
-    farePerKilo: 2,
-    capacity: 2,
-    description: "Bike ride is my medicine. This weather is best for Bike riding. I just need a bike ride. Let's play a bike race game.",
+  vehicle: "Car",
+  imageUrl: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  farePerKilo: 3,
+  capacity: 4,
+  description: "You Have More Flexibility And Freedom. When you travel by car, you are on your schedule and have complete control over your trip.",
 };
 var busObject = {
-    vehicle: "Bus",
-    imageUrl: "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    farePerKilo: 3,
-    capacity: 30,
-    description: "In the long boring bus journey sleeping on friends shoulder, Sharing earphones then fighting over which song has to play next",
+  vehicle: "Bus",
+  imageUrl: "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  farePerKilo: 3,
+  capacity: 30,
+  description: "In the long boring bus journey sleeping on friends shoulder, Sharing earphones then fighting over which song has to play next",
+};
+var bikeObject = {
+  vehicle: "Bike",
+  imageUrl: "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+  farePerKilo: 3,
+  capacity: 30,
+  description: "In the long boring bus journey sleeping on friends shoulder, Sharing earphones then fighting over which song has to play next",
 };
 
-function displayServices(service) {
-    const mainSection = document.getElementById("main-section");
-    const stringifiedObj = JSON.stringify(service);
-    const div = document.createElement("div");
+const servicesArray = [carObject, bikeObject, busObject];
 
-    div.innerHTML = `
+function displayServices(service) {
+  const mainSection = document.getElementById("main-section");
+  const stringifiedObj = JSON.stringify(service);
+  const div = document.createElement("div");
+
+  div.innerHTML = `
      <div class="card mt-3  mx-auto" style="max-width: 800px;">
           <div class="row g-0">
             <div class="col-md-4">
@@ -38,25 +40,30 @@ function displayServices(service) {
                 <p class="card-text"><small class="text-muted">Fare per kilo : $${service.farePerKilo}</small> <small class="text-muted">Capacity : ${service.capacity}</small></p>
                 <!-- Button trigger modal -->
           <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick='handleBooking(${stringifiedObj})' data-bs-target="#exampleModal">
-                See Details
+                Book Now
           </button>
               </div>
             </div>
           </div>
   </div>
   `;
-    mainSection.appendChild(div);
+  mainSection.appendChild(div);
 }
 
-displayServices(carObject);
-displayServices(bikeObject);
-displayServices(busObject);
+function displayAllArticles(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    displayServices(element);
+  }
+}
+
+displayAllArticles(servicesArray);
 
 function handleBooking(obj) {
-    const modalBody = document.getElementById("modal-body");
-    const stringifiedObj = JSON.stringify(obj);
+  const modalBody = document.getElementById("modal-body");
+  const stringifiedObj = JSON.stringify(obj);
 
-    modalBody.innerHTML = ` 
+  modalBody.innerHTML = ` 
     <div class="card mx-auto w-100" style="width: 18rem;">
     <img src=${obj.imageUrl} class="card-img-top" alt="...">
     <div class="card-body">
@@ -82,21 +89,35 @@ function calculateCost(obj) {
   const quantity = document.getElementById("quantity-input").value;
   const distance = document.getElementById("distance-input").value;
 
-  if(quantity>0 && distance>0){
-  const fareDiv = document.getElementById("fare");
-  const fare = quantity * distance * obj.farePerKilo;
-  fareDiv.innerText = '$' + fare;
+  if (quantity > 0 && distance > 0) {
+    const fareDiv = document.getElementById("fare");
+    const fare = quantity * distance * obj.farePerKilo;
+    fareDiv.innerText = '$' + fare;
 
-  const taxDiv = document.getElementById("tax");
-  const tax = (fare * 0.1).toFixed(2);
-  taxDiv.innerText = '$' + tax;
+    const taxDiv = document.getElementById("tax");
+    const tax = (fare * 0.1).toFixed(2);
+    taxDiv.innerText = '$' + tax;
 
-  const totalCostDiv = document.getElementById("total-cost");
-  const totalCost = fare + tax;
-  totalCostDiv.innerText = '$' + totalCost;
-  }
-  else{
+    const totalCostDiv = document.getElementById("total-cost");
+    const totalCost = fare + tax;
+    totalCostDiv.innerText = '$' + totalCost;
+  } else {
     alert('Enter positive number');
     return;
   }
 }
+
+
+document.getElementById("search-btn").addEventListener("click", function () {
+  const value = document.getElementById("search-value").value;
+  for (let i = 0; i < servicesArray.length; i++) {
+    const element = servicesArray[i];
+    if (value.toLowerCase() ==
+      element.vehicle.toLowerCase()) {
+      document.getElementById("main-section").innerHTML = ""
+      displayServices(element)
+      return;
+    }
+  }
+  alert("Nothing Found");
+});
